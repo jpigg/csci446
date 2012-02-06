b5ec627fb418487c602f41ea4f1f3375c5409c32
Jon Pigg <jpigg@mymail.mines.edu>
2012-02-05 17:35:07 -0700

    Completed Task D, Cart Creation

--- Project03/depot/app/models/cart.rb
+++ Project03/depot/app/models/cart.rb
@@ -6,16 +6,6 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class ApplicationController < ActionController::Base
-  protect_from_forgery
-
-  private
-
-    def current_cart 
-      Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      cart
-    end
+class Cart < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
 end
