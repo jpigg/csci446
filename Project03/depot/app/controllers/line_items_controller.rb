39dc8383deac94c741eb31c4b43a43caeb810dbb
Jon Pigg <jpigg@mymail.mines.edu>
2012-02-05 20:24:50 -0700

    Completed Iteration F3, Highlighting changes

--- Project03/depot/app/controllers/line_items_controller.rb
+++ Project03/depot/app/controllers/line_items_controller.rb
@@ -51,11 +51,10 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
-        format.js
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
