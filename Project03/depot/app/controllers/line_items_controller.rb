72775565e8071a7a2e1de8e4d6e5f3f18436e347
Jon Pigg <jpigg@mymail.mines.edu>
2012-02-05 20:16:25 -0700

    Completed Iteration F2, ajax based cart

--- Project03/depot/app/controllers/line_items_controller.rb
+++ Project03/depot/app/controllers/line_items_controller.rb
@@ -51,9 +51,11 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
+
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
