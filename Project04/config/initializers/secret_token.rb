9633fe0b07bc8fc27a2fa2abf6e0a1e56d89e614
Jon Pigg <jpigg@mymail.mines.edu>
2012-02-18 01:21:17 -0700

    Added initial Project04 files

---
+++ Project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = 'bc8f603a8c1ef58e44c6b524f1b34f7add59695de0e475070233d68295595188db7c2796c941f3a14ceca5e91c50a4e0c80e18051d70b07f3c569999a366f027'
---
+++ test/someapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Someapp::Application.config.secret_token = 'e5aae5f00c0e83971913aeda0847c2975a7095a2ffe875b15fefe8151acae4f871984f8acf0ba4b287b1f0249a06acd2f65fdf5c8212769387e76f8d43bfe782'
