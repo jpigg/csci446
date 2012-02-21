8bf0ada930c6b869ac45acd846357245230e3174
Jon Pigg <jpigg@mymail.mines.edu>
2012-02-20 22:00:23 -0700

    Most functional requirements are complete

--- Project04/app/models/article.rb
+++ Project04/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+	validates :title, :author_name, :body, presence: true
+	validates_exclusion_of :author_name, :in => %w( Pat ), :message => "The name Pat is not allowed, Pat broke my heart"
 end
