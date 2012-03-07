a8fa3f71a8c13bbea058a5036852f4e7391e34e1
Jon Pigg <jpigg@mymail.mines.edu>
2012-03-07 06:46:19 -0700

    Converted to haml and sass files

--- project05/config/routes.rb
+++ project05/config/routes.rb
@@ -1,6 +1,7 @@
 Articles::Application.routes.draw do
   resources :articles
-
+	
+#	map.root :controller => 'articles'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
