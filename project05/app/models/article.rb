64ff17103d136abf8ba6ac0385220ffc05867556
Jon Pigg <jpigg@mymail.mines.edu>
2012-03-08 19:49:25 -0700

    Still chugging away

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+	belongs_to :author
 	validates :title, :author_name, :body, :creation_date, presence: true
 	validates_exclusion_of :author_name, :in => %w( Pat ), :message => "The name Pat is not allowed, Pat broke my heart"
 end
--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
-	
+	has_many :articles
 	has_attached_file :photo
 end
