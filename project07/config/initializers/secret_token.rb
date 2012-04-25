4749098e671a4b98095b73680d5ee6463fc0447c
Jon Pigg <jpigg@mymail.mines.edu>
2012-04-25 01:50:57 -0600

    Originally misunderstood assignment, after rereading started over. Basic functionality works, ie user has 10 guesses, will decrement each time, too high, too low, simple messages for now.

--- project07/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Game::Application.config.secret_token = '2a1c59a76eb713f7d11100024e676472148769a31bd198c5ada65411af9954b080b7f1de68c3bb018eff4850842e8ebf535b516c9b8d9e9889c915b6b85c1021'
--- project07/main.js
+++ project07/main.js
@@ -1,10 +1,61 @@
 var guessesLeft = 10;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
+var setNum = 50;
 
 $(function() {
   updateScore(guessesLeft);
   populateHighScores(highScores);
 });
+
+function updateGuesses()
+{
+	//document.getElementById("guessesLeft").innerHTML = guessesLeft;
+	guessesLeft = guessesLeft -1;
+	$("#guessesLeft").html(guessesLeft);
+	if(guessesLeft === 0)
+	{
+		lose();
+	}
+}
+
+function tooHigh()
+{
+	alert("too high");
+}
+
+function tooLow()
+{
+	alert("too low");
+}
+
+function lose()
+{
+	alert("You lose");
+}
+
+function win()
+{
+	alert("You win");
+}
+
+function checkGuess()
+{
+	var guess = $('#guess').val();
+	if(guess < setNum)
+	{
+		tooLow();
+	}
+	else if(guess > setNum)
+	{
+		tooHigh();
+	}
+	else
+	{
+		win();
+	}
+	updateGuesses();
+//	alert("test");
+}
 
 function populateHighScores(scores) {
   for (var i = 0; i < scores.length; ++i) {
